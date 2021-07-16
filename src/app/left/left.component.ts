937eacf9ea0e12f855dc73d70aef770f5e606701
arkbogda <ark.bogda@gmail.com>
2021-07-16 12:51:14 +0200

    BehaviorSubject and Unsubscribe

--- src/app/left/left.component.ts
+++ src/app/left/left.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
+import {Person} from "../services/data-base.service";
+import {DataService} from "../services/data.service";
+import { Subscription } from "rxjs";
 
 @Component({
   selector: 'app-left',
@@ -6,13 +9,24 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
   styleUrls: ['./left.component.css'],
 })
 export class LeftComponent implements OnInit, OnDestroy {
+  persons: Person[];
+  private subscriptions = new Subscription();
 
-  constructor() {}
+  constructor(private dataService: DataService) {}
 
   ngOnInit(): void {
-
+    const sub = this.dataService.behaviorSubject.subscribe(
+      (data: Person[]) => {
+        this.persons = data;
+        console.log('Left Component Subscription ')
+      },
+      error => console.error(error),
+      () => console.log('LeftComponent complete?')
+    )
+    this.subscriptions.add(sub);
   }
 
   ngOnDestroy(): void {
+    this.subscriptions.unsubscribe();
   }
 }
